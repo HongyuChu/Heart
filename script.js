634c108743002dc2e8e07ead91c9cb8ba5106c01
HongyuChu <129086704+HongyuChu@users.noreply.github.com>
2023-08-17 01:11:02 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login() {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
-    if (username === "user123" && password === "pass123") {
+    if (username === "smy521" && password === "chy521") {
         document.getElementById('login-container').style.display = 'none';
         document.getElementById('heart-container').style.display = 'flex';
     } else {
